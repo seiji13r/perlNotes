9866b4b5cfbee4ea5489ec08b3d3809fabe38e50
A. Seiji Osada V <seiji13r@outlook.com>
2019-10-11 18:25:43 -0500

    small change

--- scripts/00references.pl
+++ scripts/00references.pl
@@ -3,7 +3,6 @@
 use strict;
 use warnings;
 
-
 my @array = (1, 2, 3, 4);             # array
 my $arrayref1 = \@array;              # array reference
 my $arrayref2 = ['a', 'b', 'c', 'd']; # array reference
